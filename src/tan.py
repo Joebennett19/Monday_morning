f34b2e83ca1b81e17b8e64c184ee0433cffc65e2
Joe <mm16j2b@leeds.ac.uk>
2021-03-15 12:09:46 +0000

    Add tan.py

---
+++ src/tan.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 15 10:38:34 2021
+
+@author: mm16j2b
+"""
+
+import matplotlib.pyplot as plt
+import numpy
+x=numpy.linspace(0,5,100)
+u=numpy.tan(x)
+fig, ax1 = plt.subplots()
+ax1.plot(x,u)
+
+plt.show()
